d5af7bbfa5719f768202103478cb2a4fa5f80f8b
javad-reazaie <111302208+JAVADking4259@users.noreply.github.com>
2024-06-30 12:00:09 -0700

    add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config'
 
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { auth } from './auth/entity/auth.ent';
 
+
 @Module({
-  imports: [AuthModule,
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true, 
+      envFilePath:'.env'
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'javadKING',
-      database: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [auth],
       synchronize: true,
     })
